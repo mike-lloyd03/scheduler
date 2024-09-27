ea6abe705df3e75e50607027acd5e7f72d4a4d07
Mike Lloyd <mike.lloyd03@pm.me>
2024-09-26 22:46:26 -0700

    Enable automigration

--- backend/main.go
+++ backend/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"log"
 	"os"
+	"strings"
 
+	_ "backend/migrations"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
 )
 
 func main() {
@@ -16,6 +19,15 @@ func main() {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
 		return nil
+	})
+
+	// loosely check if it was executed using "go run"
+	isGoRun := strings.HasPrefix(os.Args[0], os.TempDir())
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		// enable auto creation of migration files when making collection changes in the Admin UI
+		// (the isGoRun check is to enable it only during development)
+		Automigrate: isGoRun,
 	})
 
 	if err := app.Start(); err != nil {
